efadbf81bd0a79e9009d5aafe99d58dab0025db6
Tyler Moore <tmoore@clubautomation.com>
2020-07-16 13:59:07 -0500

    Update non-ts version

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-import React              from 'react';
-import { render }         from 'react-dom';
-import { Provider }       from 'react-redux';
-import store              from 'SRC/store';
-import {
-  BrowserRouter,
-  Route,
-  Switch
-}                         from 'react-router-dom';
+import React from 'react';
+import App from './App';
+import { render } from 'react-dom';
 
 render((
-  <Provider store={ store }>
-    <BrowserRouter>
-      <Switch>
-        <Route render={ () => <h1>Hello World</h1>} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>
+  <App />
 ), document.getElementById('root'));
